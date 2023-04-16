d1259b82e8aa5b4882236cb369230bad10a779a0
Daniil Solynin <didhat82@gmail.com>
2023-04-16 17:59:14 +0300

    refractor

--- src/models/finance/finance.go
+++ src/models/finance/finance.go
@@ -1,17 +1,27 @@
 package finance
 
+import "main/src/entrypoints/webmodels"
+
 type Spending struct {
-	UserId int
-	Name   string
-	Type   string
+	UserId   int
+	Name     string
+	Type     string
+	Amount   int
+	Currency string
 }
 
 type FactoryImp struct{}
 
-func (fi FactoryImp) CreateSpending(userId int, name string, Type string) Spending {
-	return Spending{Name: name, Type: Type, UserId: userId}
+func SpendingFromUserInput(spending webmodels.TestSpending, userId int) Spending {
+	return Spending{UserId: userId, Name: spending.Name, Type: spending.Type,
+		Amount:   spending.Amount,
+		Currency: spending.Currency}
+}
+
+func (fi FactoryImp) CreateSpending(userId int, name string, Type string, amount int, currency string) Spending {
+	return Spending{Name: name, Type: Type, UserId: userId, Amount: amount, Currency: currency}
 }
 
 type Factory interface {
-	CreateSpending(userId int, name string, Type string) Spending
+	CreateSpending(userId int, name string, Type string, amount int, currency string) Spending
 }
--- src/repo/finance.go
+++ src/repo/finance.go
@@ -5,72 +5,21 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 	"main/src/models/finance"
-	"sync"
 )
-
-type MemoryFinanceRepository struct {
-	spends map[int]finance.Spending
-	lock   *sync.RWMutex
-
-	financeFactory finance.Factory
-}
-
-var idForSpend int = 1
-
-func NewMemoryFinanceRepository() MemoryFinanceRepository {
-	return MemoryFinanceRepository{
-		spends:         map[int]finance.Spending{},
-		lock:           &sync.RWMutex{},
-		financeFactory: finance.FactoryImp{},
-	}
-}
-
-func (mfr MemoryFinanceRepository) CreateFinanceSpending(ctx context.Context, userId int, name string) error {
-	mfr.lock.RLock()
-	defer mfr.lock.RUnlock()
-
-	newSpend := mfr.financeFactory.CreateSpending(userId, name, "common")
-	mfr.spends[idForSpend] = newSpend
-	idForSpend++
-	return nil
-}
-
-func (mfr MemoryFinanceRepository) DeleteFinanceSpending(ctx context.Context, userId int, id int) error {
-	mfr.lock.RLock()
-	defer mfr.lock.RUnlock()
-
-	delete(mfr.spends, id)
-
-	return nil
-}
-
-func (mfr MemoryFinanceRepository) GetUserFinanceSpends(ctx context.Context, userId int) (error, []finance.Spending) {
-	userSpends := make([]finance.Spending, 0, 10)
-
-	for _, spend := range mfr.spends {
-		if spend.UserId == userId {
-			userSpends = append(userSpends, spend)
-		}
-	}
-
-	return nil, userSpends
-}
 
 type PostgresFinanceRepository struct {
 	pool *pgxpool.Pool
-
-	financeFactory finance.Factory
 }
 
 func NewPostgresFinanceRepository(pool *pgxpool.Pool) PostgresFinanceRepository {
 	return PostgresFinanceRepository{
-		pool:           pool,
-		financeFactory: finance.FactoryImp{},
+		pool: pool,
 	}
 }
 
-func (pfr PostgresFinanceRepository) CreateFinanceSpending(ctx context.Context, userId int, name string) error {
-	_, err := pfr.pool.Exec(ctx, "INSERT INTO spends(name, type, user_id) values ($1, $2, $3)", name, "test", userId)
+func (pfr PostgresFinanceRepository) CreateFinanceSpending(ctx context.Context, userId int, spending finance.Spending) error {
+	_, err := pfr.pool.Exec(ctx, "INSERT INTO spend(name, type, user_id) values ($1, $2, $3)",
+		spending.Name, spending.Type, userId)
 	if err != nil {
 		return err
 	}
@@ -81,14 +30,15 @@ func (pfr PostgresFinanceRepository) CreateFinanceSpending(ctx context.Context, 
 func (pfr PostgresFinanceRepository) GetUserFinanceSpends(ctx context.Context, userId int) (error, []finance.Spending) {
 	spends := make([]finance.Spending, 0, 30)
 
-	rows, err := pfr.pool.Query(ctx, "SELECT name, type FROM spends WHERE user_id=$1", userId)
+	rows, err := pfr.pool.Query(ctx, "SELECT name, type, COALESCE(amount, 0) FROM spend WHERE user_id=$1", userId)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for rows.Next() {
 		var sp finance.Spending
 		sp.UserId = userId
-		err = rows.Scan(&sp.Name, &sp.Type)
+		err = rows.Scan(&sp.Name, &sp.Type, &sp.Amount)
 		spends = append(spends, sp)
 	}
 
@@ -100,7 +50,7 @@ func (pfr PostgresFinanceRepository) DeleteFinanceSpending(ctx context.Context, 
 }
 
 type FinanceRepository interface {
-	CreateFinanceSpending(ctx context.Context, userId int, name string) error
-	DeleteFinanceSpending(ctx context.Context, userId int, id int) error
+	CreateFinanceSpending(ctx context.Context, userId int, spending finance.Spending) error
 	GetUserFinanceSpends(ctx context.Context, userId int) (error, []finance.Spending)
+	DeleteFinanceSpending(ctx context.Context, userId int, id int) error
 }
