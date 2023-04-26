9b06fdc248a6e63eb472143458e90c32794e6180
Daniil Solynin <didhat82@gmail.com>
2023-04-26 10:39:45 +0300

    add description to model

--- finances/models/finance/finance.go
+++ finances/models/finance/finance.go
@@ -6,21 +6,26 @@ import (
 )
 
 type Spending struct {
-	Id       int
-	UserId   int
-	Name     string
-	Type     string
-	Amount   int
-	Currency string
-	Time     time.Time
+	Id          int
+	UserId      int
+	Name        string
+	Type        string
+	Amount      int
+	Currency    string
+	Description string
+	Time        time.Time
 }
 
 type FactoryImp struct{}
 
 func SpendingFromUserInput(spending webmodels.TestSpending, userId int) Spending {
-	return Spending{UserId: userId, Name: spending.Name, Type: spending.Type,
-		Amount:   spending.Amount,
-		Currency: spending.Currency}
+	return Spending{
+		UserId:      userId,
+		Name:        spending.Name,
+		Type:        spending.Type,
+		Amount:      spending.Amount,
+		Currency:    spending.Currency,
+		Description: spending.Description}
 }
 
 func (fi FactoryImp) CreateSpending(userId int, name string, Type string, amount int, currency string) Spending {
