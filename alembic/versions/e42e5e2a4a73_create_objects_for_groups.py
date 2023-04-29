c3b129cda8be9e3087074fedb6097047682b4029
Daniil Solynin <didhat82@gmail.com>
2023-04-29 20:53:18 +0300

    add migrations for groups

---
+++ alembic/versions/e42e5e2a4a73_create_objects_for_groups.py
@@ -0,0 +1,38 @@
+"""create objects for groups
+
+Revision ID: e42e5e2a4a73
+Revises: 09d95af18278
+Create Date: 2023-04-29 19:19:43.708715
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "e42e5e2a4a73"
+down_revision = "09d95af18278"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "group",
+        sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("name", sa.String, nullable=False),
+        sa.Column("description", sa.String)
+    ),
+
+    op.add_column("spend", sa.Column("group_id", sa.Integer))
+    op.create_foreign_key(
+        None,
+        "spend",
+        "group",
+        ["group_id"],
+        ["id"]
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("spend", "group_id")
+    op.drop_table("group")
