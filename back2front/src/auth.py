f65579fdd4695722e0f482e011acd3ed3abdf924
Daniil Solynin <didhat82@gmail.com>
2023-05-07 18:38:09 +0300

    move to env for back2front

--- back2front/src/auth.py
+++ back2front/src/auth.py
@@ -2,12 +2,15 @@ import jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import APIKeyHeader
 
+from src.config import get_secret_key
+
+
 api_token_header = APIKeyHeader(name="Auth-Token")
 
 
 def auth_header_token(token: str = Depends(api_token_header)) -> tuple[int, str]:
     try:
-        info = jwt.decode(token, "secretKey", algorithms=["HS256"])
+        info = jwt.decode(token, get_secret_key(), algorithms=["HS256"])
     except Exception as ex:
         print(ex)
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
