1eb1675a191d6e23ff6db53954d6028ffa478e8e
Daniil Solynin <didhat82@gmail.com>
2023-05-08 14:57:34 +0300

    add env for notification service

--- notifications/main.go
+++ notifications/main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
+	"log"
 	"main/finances/entrypoints/middleware"
 	"main/notifications/api"
 	"main/notifications/resolver"
 	"net/http"
+	"os"
+	"strconv"
 )
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
+	redisdb, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+
+	if err != nil {
+		log.Print(err)
+	}
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       redisdb,
 	})
+
+	startPort := os.Getenv("NOTIFICATION_STARTUP_PORT")
 
 	eventResolver := resolver.EventsResolver{Redis: rdb}
 
@@ -29,6 +47,6 @@ func main() {
 	mux.Handle("/set", http.HandlerFunc(entrypoint.SetNotificationStatus))
 	mux.Handle("/get", http.HandlerFunc(entrypoint.GetNotificationUserStatus))
 
-	http.ListenAndServe(":4005", middleware.AuthMiddleware(mux))
+	http.ListenAndServe(fmt.Sprintf(":%s", startPort), middleware.AuthMiddleware(mux))
 
 }
